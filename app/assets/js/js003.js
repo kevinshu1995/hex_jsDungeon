642df24ba852bfd4a9f40a16f44e80d55c7626f1
Kevin Hsu <kevin_hsu@joy.com.tw>
2020-06-01 17:04:56 +0800

    解決按"="之後如果沒有算式會出現undefined

--- app/assets/js/js003.js
+++ app/assets/js/js003.js
@@ -2,9 +2,11 @@ let btns = document.querySelector('.btnsWrap');
 let currNum = document.querySelector('.currNum');
 let currSent = document.querySelector('.currSent');
 
+//現在剛按下去的按鈕(大字、白色的那個)， eg: a+b=c  也就是b
 let numStr = '';
+//基本上只會短暫的在按"="後有值，function結束就會清除
 let Ans = '';
-//運算式
+//就是藍色的那個計算式
 let cal = [];
 
 //判斷如果有按過 "=" 則為ture，進入function清除計算欄後 checkEqual 變回 false
@@ -47,19 +49,24 @@ function calculate(valNum, valStr) {
 			currSent.innerHTML = '';
 			Ans = '';
 		}
+
 		//findComma -> 若有'.'則回傳'.'
 		let findComma = numStr.split('').find(function (item) {
 			return item === "."
 		});
-		//若已經包含'.'且按的是'.'，則不繼續執行
+
+		//若vlaStr已經包含'.'且按的是'.'，則不繼續執行
 		if (findComma === "." && valStr === '.') return
-		if (Number(valStr) === 0 && numStr == 0) {
+
+		//避免000.00的狀況發生
+		if (Number(valStr) === 0 && numStr == 0 && findComma !== '.') {
 			numStr = '0';
 		} else {
 			//加入點擊按鈕進入numStr
 			numStr = numStr += valStr;
 		}
 		currNum.innerHTML = numStr;
+
 	} else if (valStr == 'AC') {  //判斷點擊'AC'，清除輸入欄
 		numStr = '';
 		Ans = '';
@@ -85,19 +92,25 @@ function calculate(valNum, valStr) {
 		numStr = '';
 	} else if (valStr == '=') {
 
+		// 沒有算式就不執行
+		if(cal.length===0) return
+		
 		//再做一次
 		if (numStr !== '') {
 			cal.push(Number(numStr));
 			//清空輸入區
 			currNum.innerHTML = '';
 		}
-		//呈現運算區
+
+		//呈現運算式子
 		let str = ''
 		cal.forEach(function (val) {
 			str += ' ' + val
 		});
 		currSent.innerHTML = str;
 		numStr = '';
+
+		//把符號轉為JS看的懂的運算符號
 		cal.forEach(function (item, index) {
 			switch (item) {
 				case "×":
