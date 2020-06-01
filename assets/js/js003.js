bdb7de04f20d82e1a8ce02f46080d0bea48fb7ef
Kevin Hsu <kevin_hsu@joy.com.tw>
2020-06-01 17:05:18 +0800

    Update 2020-06-01T09:05:12.569Z

--- assets/js/js003.js
+++ assets/js/js003.js
@@ -2,112 +2,135 @@
 
 var btns = document.querySelector('.btnsWrap');
 var currNum = document.querySelector('.currNum');
-var currSent = document.querySelector('.currSent');
-var numStr = '';
-var Ans = ''; //運算式
+var currSent = document.querySelector('.currSent'); //現在剛按下去的按鈕(大字、白色的那個)， eg: a+b=c  也就是b
 
-var cal = [];
+var numStr = ''; //基本上只會短暫的在按"="後有值，function結束就會清除
+
+var Ans = ''; //就是藍色的那個計算式
+
+var cal = []; //判斷如果有按過 "=" 則為ture，進入function清除計算欄後 checkEqual 變回 false
+
+var checkEqual = false;
+
+function clearCalculation() {
+  if (!checkEqual) return;
+  currSent.innerHTML = '';
+  checkEqual = false;
+}
+
 btns.addEventListener('click', count);
 
 function count(e) {
   //判斷點擊是否為LI
-  if (e.target.nodeName === 'LI') {
-    //字串轉為數字
-    var valNum = Number(e.target.textContent); //點擊的字串
+  if (e.target.nodeName !== 'LI') return; //字串轉為數字
 
-    var valStr = e.target.textContent; //判斷點擊若為數字或'.'則繼續執行
+  var valNum = Number(e.target.textContent); //點擊的字串
 
-    if (!isNaN(valNum) || valStr == '.') {
-      if (Ans !== '') {
-        currSent.innerHTML = '';
-        Ans = '';
-      } //findComma -> 若有'.'則回傳'.'
-
-
-      var findComma = numStr.split('').find(function (item) {
-        return item === ".";
-      }); //若已經包含'.'且按的是'.'，則不繼續執行
-
-      if (findComma === "." && valStr === '.') {
-        return;
-      }
-
-      if (Number(valStr) === 0 && numStr == 0) {
-        numStr = '0';
-      } else {
-        //加入點擊按鈕進入numStr
-        numStr = numStr += valStr;
-      }
-
-      currNum.innerHTML = numStr;
-    } else if (valStr == 'AC') {
-      //判斷點擊'AC'，清除輸入欄
-      numStr = '';
-      currNum.innerHTML = '';
-      currNum.innerHTML = numStr;
-    } else if (valStr === '÷' || valStr === '×' || valStr === '+' || valStr === '−') {
-      //加入陣列
-      if (numStr === '') {
-        cal[cal.length - 1] = valStr;
-      } else {
-        cal.push(Number(numStr), valStr); //清空輸入區
-
-        currNum.innerHTML = '';
-      } //呈現運算區
-
-
-      var str = '';
-      cal.forEach(function (val) {
-        str += ' ' + val;
-      });
-      currSent.innerHTML = str;
-      numStr = '';
-    } else if (valStr == '=') {
-      //再做一次
-      if (numStr !== '') {
-        cal.push(Number(numStr)); //清空輸入區
-
-        currNum.innerHTML = '';
-      } //呈現運算區
-
-
-      var _str = '';
-      cal.forEach(function (val) {
-        _str += ' ' + val;
-      });
-      currSent.innerHTML = _str;
-      numStr = '';
-      cal.forEach(function (item, index) {
-        switch (item) {
-          case "×":
-            cal[index] = '*';
-            break;
-
-          case "÷":
-            cal[index] = '/';
-            break;
-
-          case "+":
-            cal[index] = '+';
-            break;
-
-          case "-":
-            cal[index] = '-';
-            break;
-        }
-      });
-      Ans = eval(cal.join(''));
-      cal = [];
-      currNum.innerHTML = Ans;
-    }
-  } //判斷點擊刪除鍵
-
+  var valStr = e.target.textContent;
+  calculate(valNum, valStr); //判斷點擊刪除鍵
 
   if (e.target.textContent == "delete" || e.target.nodeName == 'I') {
     var ary = numStr.split('');
     ary.splice(-1, 1);
     numStr = ary.join('');
     currNum.innerHTML = numStr;
+  }
+}
+
+function calculate(valNum, valStr) {
+  //判斷點擊若為數字或'.'則繼續執行
+  if (!isNaN(valNum) || valStr == '.') {
+    // 判斷是否刪除運算式
+    clearCalculation();
+
+    if (Ans !== '') {
+      currSent.innerHTML = '';
+      Ans = '';
+    } //findComma -> 若有'.'則回傳'.'
+
+
+    var findComma = numStr.split('').find(function (item) {
+      return item === ".";
+    }); //若vlaStr已經包含'.'且按的是'.'，則不繼續執行
+
+    if (findComma === "." && valStr === '.') return; //避免000.00的狀況發生
+
+    if (Number(valStr) === 0 && numStr == 0 && findComma !== '.') {
+      numStr = '0';
+    } else {
+      //加入點擊按鈕進入numStr
+      numStr = numStr += valStr;
+    }
+
+    currNum.innerHTML = numStr;
+  } else if (valStr == 'AC') {
+    //判斷點擊'AC'，清除輸入欄
+    numStr = '';
+    Ans = '';
+    cal = [];
+    currNum.innerHTML = '';
+    currSent.innerHTML = '';
+  } else if (valStr === '÷' || valStr === '×' || valStr === '+' || valStr === '−') {
+    //加入陣列
+    if (cal.length === 0 && !numStr) return;
+
+    if (numStr === '') {
+      cal[cal.length - 1] = valStr;
+    } else {
+      cal.push(Number(numStr), valStr); //清空輸入區
+
+      currNum.innerHTML = '';
+    } //呈現運算區
+
+
+    var str = '';
+    cal.forEach(function (val) {
+      str += ' ' + val;
+    });
+    currSent.innerHTML = str;
+    numStr = '';
+  } else if (valStr == '=') {
+    // 沒有算式就不執行
+    if (cal.length === 0) return; //再做一次
+
+    if (numStr !== '') {
+      cal.push(Number(numStr)); //清空輸入區
+
+      currNum.innerHTML = '';
+    } //呈現運算式子
+
+
+    var _str = '';
+    cal.forEach(function (val) {
+      _str += ' ' + val;
+    });
+    currSent.innerHTML = _str;
+    numStr = ''; //把符號轉為JS看的懂的運算符號
+
+    cal.forEach(function (item, index) {
+      switch (item) {
+        case "×":
+          cal[index] = '*';
+          break;
+
+        case "÷":
+          cal[index] = '/';
+          break;
+
+        case "+":
+          cal[index] = '+';
+          break;
+
+        case "−":
+          cal[index] = '-';
+          break;
+      }
+    });
+    Ans = eval(cal.join(''));
+    currNum.innerHTML = Ans;
+    Ans = '';
+    cal = [];
+    checkEqual = true;
   }
 }
 //# sourceMappingURL=js003.js.map
