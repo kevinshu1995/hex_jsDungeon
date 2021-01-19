c3b06c0d2c43415ed3e37e87b3cb430f685288e6
kevinHsu <kevinshu1995@gmail.com>
2021-01-19 22:30:39 +0800

    Refactory

--- app/assets/js/js003.js
+++ app/assets/js/js003.js
@@ -1,204 +1,4 @@
-// {
-//     act: '加減乘除',
-//     number: '10234.8',
-// }
-class Calculate {
-    constructor(){
-        this.el_btnWrap = document.querySelector('.btnsWrap')
-        this.el_newNumber = document.querySelector('.currNum')
-        this.newAction = document.querySelector('.currSent')
-        // [{ act:'division', number: '10.2' }]
-        this.calculate = []
-        // ['1','2','.','2']
-        this.current_Number = []
-    }
-    init(){
-        if(this.el_btnWrap)
-            this.el_btnWrap.addEventListener('click', this.count.bind(this))
-    }
-    format_marks(key){
-        const mark = {
-            'division': '÷',
-            'times': '×',
-            'plus': '+',
-            'minus': '-',
-        }
-        return mark[key]
-    }
-    // return Array
-    get formula(){
-        if(this.calculate.length === 0) return ''
-        return this.calculate.map(item => `${item.number} ${this.format_marks(item.act)}` )
-    }
-    get_number(num = ''){
-        const result = []
-        if(num === 'd-0') 
-            result.push('0', '0') 
-        else 
-            result.push(num)
-        return result
-    }
-    get_point(original=[]){
-        let point = () => original.find(item=> item === '.' )
-        let result = [...original]
-        if(original.length===0)
-            result.push('0','.')
-        else
-            if( point() === undefined ) result.push('.')
-        return result
-    }
-    get_new_calculate(mark){
-        const c_num = this.current_Number
-        let new_number = c_num[c_num.length] === '.' ? c_num : ''
-        return {       
-            act: mark,
-            number: new_number,
-        }
-    }
-    clear(target){
-        this[target] = []
-    }
-    delete(action){
-        return action    
-    }
-    equal(action){
-        return action    
-    }
-    count(e) {
-        //判斷點擊是否為LI
-        let vm = this
-        if(e.target.dataset.action === undefined) return 
-        let action = e.target.dataset.action
-        switch(action){
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-            case 'd-0':
-                vm.current_Number = [...vm.current_Number, ...vm.get_number(action)]
-                console.log('number',vm.current_Number)
-                break;
-            case 'point':
-                vm.current_Number = vm.get_point(vm.current_Number)
-                console.log('number',vm.get_point(vm.current_Number))
-                break;
-            case 'division':
-            case 'times':
-            case 'plus':
-            case 'minus':
-                vm.calculate.push(vm.get_new_calculate(action))
-                vm.clear('current_Number')
-                console.log('math', vm.current_Number, vm.calculate)
-                break;
-            case 'ac':
-            case 'delete':
-                console.log('delete', vm.delete(action))
-                break;
-            case 'equal':
-                console.log('equal', vm.equal(action))
-                break;
-        }
-        console.log(vm.formula)
-        // calculate(value)
-    
-        //判斷點擊刪除鍵
-        // if(e.target.classList)
-        // if (e.target.textContent == 'delete' || e.target.nodeName == 'I') {
-        //     value = numStr.split('').splice(-1, 1).join('')
-        //     //innerHTML// currNum.innerHTML = numStr
-        // }
-    }
-}
 
-let cal = new Calculate()
-cal.init()
-
-// let btnWrap = document.querySelector('.btnsWrap')
-// let currNum = document.querySelector('.currNum')
-// let currSent = document.querySelector('.currSent')
-
-// //現在剛按下去的按鈕(大字、白色的那個)， eg: a+b=c  也就是b
-// let numStr = ''
-// //基本上只會短暫的在按"="後有值，function結束就會清除
-// let Ans = ''
-// //就是藍色的那個計算式
-// let cal = []
-
-// //判斷如果有按過 "=" 則為ture，進入function清除計算欄後 checkEqual 變回 false
-// let checkEqual = false
-// function clearCalculation() {
-//     if (!checkEqual) return
-//     currSent.innerHTML = ''
-//     checkEqual = false
-// }
-// let calculation = {
-//     dom: {
-//         btnWrap: document.querySelector('.btnsWrap'),
-//         newNumber: document.querySelector('.currNum'),
-//         newAction: document.querySelector('.currSent'),
-//     },
-//     marks: {
-//         'division': '÷',
-//         'times': '×',
-//         'plus': '+',
-//         'minus': '-',
-//     },
-//     get formula(){
-//         return this.return_formula()
-//     },
-//     calculate: [],
-//     temp_calculate: [],
-//     // {
-//     //     act: '加減乘除',
-//     //     number: '10234.8',
-//     // }
-//     return_formula: function(){
-//         let vm = this
-//         if(vm.calculate.length === 0) return ''
-//         return vm.calculate.map(item => 
-//             `${item.number} ${vm.calculation}`
-//         )
-//     },
-//     number: function(action){
-//         let cal = this.temp_calculate
-//         if(action === 'd-0'){
-//             cal.push('0', '0') 
-//         }
-//         else
-//             cal.push(action)
-//     },
-//     point: function(action){
-//         let cal = this.temp_calculate
-//         if(cal.length===0){ 
-//             cal.push('0','.')
-//         }
-//         else{
-//             let point = cal.find(item=> item === '.' )
-//             if( point === undefined ) cal.push('.')
-//         }
-//     },
-//     actions: function(action){
-//         this.calculate.push({
-//             act: action,
-//             number: this.temp_calculate,
-//         })
-//         this.temp_calculate = []
-//     },
-//     delete: function(action){
-//         return action
-//     },
-//     equal: function(action){
-//         return action
-//     },
-// }
-// btnWrap.addEventListener('click', count)
-// * 判斷點誰 => 做甚麼是
 /** 
  *  * 變數
  *      * 算式
@@ -215,149 +15,224 @@ cal.init()
  *              * 舊字串無'.' => '舊字串', '.'
  *              * 舊字串有'.' => '舊字串'
  */
-// function count(e) {
-//     //判斷點擊是否為LI
-//     if(e.target.dataset.action === undefined) return 
-//     let action = e.target.dataset.action
-//     switch(action){
-//         case '0':
-//         case '1':
-//         case '2':
-//         case '3':
-//         case '4':
-//         case '5':
-//         case '6':
-//         case '7':
-//         case '8':
-//         case '9':
-//         case 'd-0':
-//             console.log('number',calculation.number(action))
-//             break;
-//         case 'point':
-//             console.log('number',calculation.point(action))
-//             break;
-//         case 'division':
-//         case 'times':
-//         case 'plus':
-//         case 'minus':
-//             console.log('actions', calculation.actions(action))
-//             break;
-//         case 'ac':
-//         case 'delete':
-//             console.log('delete', calculation.delete(action))
-//             break;
-//         case 'equal':
-//             console.log('equal', calculation.equal(action))
-//             break;
-//     }
-//     console.log(calculation.formula)
-//     // calculate(value)
+class Calculate {
+    constructor(wrap_selector){
+        this.el_btnWrap = wrap_selector
+        // * eg: [{ act:'division', number: '10.2' }]
+        this.calculate_objects = []
+        // * eg: ['1','2','.','2']
+        this.current_Numbers_array = []
+        this.answer = null
+        this.is_done_calculating = false
+    }
 
-//     //判斷點擊刪除鍵
-//     // if(e.target.classList)
-//     // if (e.target.textContent == 'delete' || e.target.nodeName == 'I') {
-//     //     value = numStr.split('').splice(-1, 1).join('')
-//     //     //innerHTML// currNum.innerHTML = numStr
-//     // }
-// }
- 
+    init(){
+        const vm = this
+        if(document.querySelector(vm.el_btnWrap))
+            document.querySelector(vm.el_btnWrap).addEventListener('click', vm.count.bind(vm))
+    }
+    
+    // return Array
+    // 顯示在最上方的公式，不包含現在正在加值的數字
+    get formula(){
+        if(this.calculate_objects.length === 0) 
+            return ''
+        else
+            return this.calculate_objects
+                .map(
+                    item => `${item.number.join('')} ${this.format_marks(item.act)} ` 
+                )
+                .join('')
+    }
 
-// function calculate(valNum, valStr) {
-//     //判斷點擊若為數字或'.'則繼續執行
-//     if (!isNaN(valNum) || valStr == '.') {
-//         // 判斷是否刪除運算式
-//         clearCalculation()
-//         if (Ans !== '') {
-//             currSent.innerHTML = ''
-//             Ans = ''
-//         }
+    get current_nums(){
+        return this.current_Numbers_array.join('')
+    }
 
-//         //findComma -> 若有'.'則回傳'.'
-//         let findComma = numStr.split('').find(function (item) {
-//             return item === '.'
-//         })
+    get get_answer(){
+        return this.answer
+    }
 
-//         //若vlaStr已經包含'.'且按的是'.'，則不繼續執行
-//         if (findComma === '.' && valStr === '.') return
+    update_elements(){
+        this.update_formula_element()
+        this.update_answer_element()
+    }
 
-//         //避免000.00的狀況發生
-//         if (Number(valStr) === 0 && numStr == 0 && findComma !== '.') {
-//             numStr = '0'
-//         } else {
-//             //加入點擊按鈕進入numStr
-//             numStr = numStr += valStr
-//         }
-//         currNum.innerHTML = numStr
-//     } else if (valStr == 'AC') {
-//         //判斷點擊'AC'，清除輸入欄
-//         numStr = ''
-//         Ans = ''
-//         cal = []
-//         currNum.innerHTML = ''
-//         currSent.innerHTML = ''
-//     } else if (
-//         valStr === '÷' ||
-//         valStr === '×' ||
-//         valStr === '+' ||
-//         valStr === '−'
-//     ) {
-//         //加入陣列
-//         if (cal.length === 0 && !numStr) return
-//         if (numStr === '') {
-//             cal[cal.length - 1] = valStr
-//         } else {
-//             cal.push(Number(numStr), valStr)
-//             //清空輸入區
-//             currNum.innerHTML = ''
-//         }
-//         //呈現運算區
-//         let str = ''
-//         cal.forEach(function (val) {
-//             str += ' ' + val
-//         })
-//         currSent.innerHTML = str
-//         numStr = ''
-//     } else if (valStr == '=') {
-//         // 沒有算式就不執行
-//         if (cal.length === 0) return
+    update_formula_element(){
+        const el_formula = document.getElementById('current_formula')
+        el_formula.textContent = this.formula
+    }
 
-//         //再做一次
-//         if (numStr !== '') {
-//             cal.push(Number(numStr))
-//             //清空輸入區
-//             currNum.innerHTML = ''
-//         }
+    update_answer_element(){
+        const el_answer = document.getElementById('current_answer')
+        if(this.current_nums === '' && this.get_answer === null)
+            el_answer.textContent = ''
+        else
+            el_answer.textContent = this.current_nums === '' ? this.get_answer : this.current_nums
+    }
 
-//         //呈現運算式子
-//         let str = ''
-//         cal.forEach(function (val) {
-//             str += ' ' + val
-//         })
-//         currSent.innerHTML = str
-//         numStr = ''
+    format_marks(key){
+        const mark = {
+            'division': '÷',
+            'times': '×',
+            'plus': '+',
+            'minus': '-',
+            'equal': '=',
+        }
+        return mark[key]
+    }
 
-//         //把符號轉為JS看的懂的運算符號
-//         cal.forEach(function (item, index) {
-//             switch (item) {
-//                 case '×':
-//                     cal[index] = '*'
-//                     break
-//                 case '÷':
-//                     cal[index] = '/'
-//                     break
-//                 case '+':
-//                     cal[index] = '+'
-//                     break
-//                 case '−':
-//                     cal[index] = '-'
-//                     break
-//             }
-//         })
+    /**
+     *  * 若傳入 0 || d-0 則判斷是否直接在 current_Numbers_array push 0 || 00
+     *  * 若非，則取代 current_Numbers_array 第一個 0，或是 push 新數字
+     *  @param {String} num 
+     *  @return {Array}
+     */
+    get_number(num = ''){
+        const result = [...this.current_Numbers_array]
+        switch(num){
+            case '0':
+            case 'd-0':
+                if(result.length === 0)
+                    return ['0']
+                else if(result[0]==='0')
+                    return ['0']
+                else if(num==='0') 
+                    return ['0']
+                else 
+                    return [...result, '0', '0']
+            default:
+                if(result.length===1)
+                    if(result[0]==='0') return [ num ]
+                return [...result, num ]
+        }
+    }
 
-//         Ans = eval(cal.join(''))
-//         currNum.innerHTML = Ans
-//         Ans = ''
-//         cal = []
-//         checkEqual = true
-//     }
-// }
+    /**
+     *  * 判斷 original = [] 是否有小數點
+     *      * 1. 長度為 0               => return 0.
+     *      * 2. 長度不為0 && 沒有"."    => push "."
+     *      * 3. 都不是就直接回傳原值
+     * 
+     *  @param  {array} original
+     *  @return {array} 
+     */
+    get_point(original=[]){
+        const point = () => original.find(item=> item === '.' )
+        if(original.length===0)
+            return ['0', '.']
+        else if( point() === undefined )
+            return [...original, '.']
+        return [...original]
+    }
+    
+    get_new_calculate_objects(mark = ''){
+        const current_nums = this.current_Numbers_array
+        const new_numbers = current_nums[current_nums.length -1] == '.' 
+            ? this.delete_last_in_ary(current_nums) 
+            : current_nums
+        return {       
+            act: mark,
+            number: new_numbers,
+        }
+    }
+
+    before_adding_number_to_current_num(){
+        const vm = this
+        vm.detect_equal_and_delete()
+        vm.is_done_calculating = false
+    }
+
+    // * 若回傳目前輸入值的物件裡的數字陣列長度不為 0，則加入儲存計算公式的物件
+    // * 並且清除目前輸入值的陣列 (this.current_Numbers_array)
+    set_formula_and_clear_current_num(mark = ''){
+        const vm = this
+        if(vm.get_new_calculate_objects(mark)['number'].length !== 0) 
+            vm.calculate_objects = [ ...vm.calculate_objects, vm.get_new_calculate_objects(mark)]
+        vm.current_Numbers_array = []
+    }
+
+    detect_equal_and_delete(){
+        if(this.is_done_calculating) {
+            this.current_Numbers_array = []
+            this.calculate_objects = []
+            this.answer = null
+        }
+    }
+
+    delete_last_in_ary(ary){
+        const result = [...ary]
+        result.pop()
+        return result
+    }
+
+    return_answer_string(){
+        const target = [...this.calculate_objects]
+        const num = (string) => Number(string)
+        return target.reduce( (accum, currentValue, currentIndex, array) =>{
+            if(currentIndex>0){
+                switch(array[currentIndex-1].act){
+                    case 'division':
+                        return num(accum) / num(currentValue.number.join(''))
+                    case 'times':
+                        return num(accum) * num(currentValue.number.join(''))
+                    case 'plus':
+                        return num(accum) + num(currentValue.number.join(''))
+                    case 'minus':
+                        return num(accum) - num(currentValue.number.join(''))
+                    default:
+                        throw '抓不到正確的運算符號 (+, -, *, /,)'
+                }
+            }else
+                return num(currentValue.number.join(''))
+        }, 0)
+    }
+
+    count(e) {
+        if(e.target.dataset.action === undefined) return 
+        const vm = this
+        const action = e.target.dataset.action
+        switch(action){
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case 'd-0':
+                vm.before_adding_number_to_current_num()
+                vm.current_Numbers_array = vm.get_number(action)
+                break;
+            case 'point':
+                vm.before_adding_number_to_current_num()
+                vm.current_Numbers_array = vm.get_point(vm.current_Numbers_array)
+                break;
+            case 'division':
+            case 'times':
+            case 'plus':
+            case 'minus':
+                vm.set_formula_and_clear_current_num(action)
+                break;
+            case 'ac':
+                vm.calculate_objects = []
+                vm.current_Numbers_array = []
+                vm.answer = null
+                break;  
+            case 'delete':
+                vm.current_Numbers_array = vm.delete_last_in_ary(vm.current_Numbers_array)
+                break;  
+            case 'equal':
+                vm.set_formula_and_clear_current_num(action)
+                vm.answer = vm.return_answer_string()
+                vm.is_done_calculating = true
+                break;
+        }
+        this.update_elements()
+    }
+}
+
+const cal = new Calculate().init()
