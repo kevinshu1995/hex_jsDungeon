4db6b4009cf2034c24b3c2a39ce44a1c04c47e2a
kevinHsu <kevinshu1995@gmail.com>
2021-02-19 00:20:29 +0800

    dropdown in js

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,45 +1,25 @@
-const navList = document.getElementById('js-navList')
-const navBar = document.getElementById('js-navBar')
+const dropdown = new Dropdown()
 
-// event
-// showNavByWidth();
-// window.addEventListener("resize", showNavByWidth);
-// navBar.addEventListener("click", showNavByClick);
-$(document).foundation()
-
-function showNavByWidth() {
-    let width = window.outerWidth
-    if (width < 768) {
-        navList.setAttribute('style', 'display: none;')
-        navBar.setAttribute('style', 'display: block;')
-    } else {
-        navList.setAttribute('style', 'display: flex;')
-        navBar.setAttribute('style', 'display: none;')
-    }
+window.onload = () => {
+    dropdown.init()
 }
 
-function showNavByClick() {
-    let getStyle = navList.getAttribute('style')
-    console.log(getStyle)
-    if (getStyle.indexOf('display: none;')) {
-        navList.setAttribute('style', 'display: none; width: auto;')
-    } else {
-        navList.setAttribute('style', 'display: block; width: 100%;')
+function Dropdown(option = {}) {
+    this.el_dropdowns = document.querySelectorAll('.dropdown')
+    this.el_dropdownsList = document.querySelectorAll('.dropdown-list')
+
+    this.init = function () {
+        this.dropdowns()
     }
-}
 
-
-// * animate
-const hover_card_enter = $('.hover--card--enter');
-$(function(){
-    hover_card_enter.on({
-        mouseleave: function () {
-            animate_hoverCard_slide($(this))
-        }
-    })
-})
-
-
-function animate_hoverCard_slide(vm){
-    vm.removeClass('hover--card--end').addClass('hover--card--end')
+    this.dropdowns = function () {
+        if (this.el_dropdowns.length <= 0 || this.el_dropdownsList <= 0) return
+        this.el_dropdowns.forEach((el_dropdown) => {
+            el_dropdown.addEventListener('click', function (e) {
+                ;[...this.children]
+                    .find((child) => child.classList.contains('dropdown-list'))
+                    .classList.toggle('block')
+            })
+        })
+    }
 }
